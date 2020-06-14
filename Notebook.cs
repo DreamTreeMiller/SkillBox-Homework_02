76858738ccc66413bd11e4acfb6d7d37fcb40d03
DreamTreeMiller <66684575+DreamTreeMiller@users.noreply.github.com>
2020-06-14 22:30:51 +0300

    Add files via upload

---
+++ Notebook.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace HomeWork_02
+{
+    /// <summary>
+    /// Организация записной книжки и действий с ней
+    /// </summary> 
+
+    class Notebook
+    {
+        /// <summary>
+        /// Список студентов
+        /// </summary>
+        public List<Student> Students { get; set; }
+
+        /// <summary>
+        ///  Конструктор, создающий список из 5 учеников
+        /// </summary>
+        public Notebook()
+        {
+            this.Students = new List<Student> ();
+            this.Students.Add(new Student("Иван", "Терентьев", 12, 150, 5, 5, 5));
+            this.Students.Add(new Student("Пётр", "Соловьёв", 11, 147, 3, 4, 5));
+            this.Students.Add(new Student("Спиридон", "Сидоров", 13, 156, 5, 3, 3));
+            this.Students.Add(new Student("Наталья", "Александрова", 13, 178, 4, 4, 4));
+            this.Students.Add(new Student("Мария", "Никоненкова", 11, 156, 3, 3, 5));
+        }
+
+        /// <summary>
+        /// Метод добавления записи (элемента списка) о новом ученике
+        /// </summary>
+        // FirstName Имя 
+        // LastName Фамилия 
+        // Age Возраст 
+        // Height Рост 
+        // History Оценка по истории 
+        // Math оценка по математике 
+        // RusLang оценка по русскому языку 
+        public void AddStudent()
+        {
+            string FirstName;
+            string LastName; 
+            byte Age;
+            byte Height;
+            byte History;
+            byte Math;
+            byte RusLang;
+
+            // Ввод данных об ученике с клавиатуры
+            // каждое поле вводим с новой строки. 
+            // В конце нажимаем возврат "Ввод"
+
+            Console.Write("Введите имя ученика: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Введите фамилию ученика: ");
+            LastName = Console.ReadLine();
+            Console.Write("Введите возраст ученика: ");
+            Age = byte.Parse(Console.ReadLine());
+            Console.Write("Введите рост ученика: ");
+            Height = byte.Parse(Console.ReadLine());
+            Console.Write("Введите оценку ученика по истории: ");
+            History = byte.Parse(Console.ReadLine());
+            Console.Write("Введите оценку ученика по математике: ");
+            Math = byte.Parse(Console.ReadLine());
+            Console.Write("Введите оценку ученика по русскому языку: ");
+            RusLang = byte.Parse(Console.ReadLine());
+
+            this.Students.Add(new Student(FirstName, LastName, Age, Height, History, Math, RusLang));
+        }
+
+        /// <summary>
+        /// Печать неформатированной зап. книжки по строкам
+        /// </summary>
+        public void PrintNoFormat()
+        {
+            Console.WriteLine("\n Печать записной книжки без форматирования\n");
+
+            foreach (var student in this.Students)      // для каждого эл-та записной книжки
+            {
+                student.OutputNoFormat();    // выводим строку неформатированных данных о студенте
+            }
+        }
+
+        /// <summary>
+        /// Печать форматированной зап. книжки по строкам
+        /// с выравниванием по правому краю каждого поля
+        /// </summary>
+        public void PrintFormat()
+        {
+            Console.WriteLine("\n Печать записной книжки с выравниванием по правому краю каждого поля\n");
+
+            foreach (var student in this.Students)      // для каждого эл-та записной книжки
+            {
+                student.OutputFormat();    // выводим строку данных о студенте. 
+                                           // выразвнивание в каждом поле по правому краю
+            }
+        }
+
+        /// <summary>
+        /// Печать форматированной зап. книжки по строкам
+        /// с выравниванием по ЛЕВОМУ краю каждого поля
+        /// данных о студенте
+        /// </summary>
+        public void PrintFormatLeft()
+        {
+            Console.WriteLine("\n Печать записной книжки с выравниванием по ЛЕВОМУ краю каждого поля\n");
+
+            foreach (var student in this.Students)      // для каждого эл-та записной книжки
+            {
+                Console.WriteLine(student.OutputFormatLeft());    // выводим строку данных о студенте
+                                                                  // выразвнивание в каждом поле по левому краю
+            }
+        }
+
+        /// <summary>
+        /// Выводит заголовок таблицы
+        /// Ширина всей выдачи 60 символа.
+        /// Имя     - 15
+        /// Фамилия - 15
+        /// Возраст -  4 
+        /// Рост    -  5 (включая начальный пробел)
+        /// История -  4
+        /// Мат-ка  -  4
+        /// Рус.яз. -  4
+        /// Ср.балл -  9
+        /// </summary>
+        public void OutputHeader()      // печатаем заголовок для центра консоли
+        {
+            Console.WriteLine("      Имя          Фамилия    " +
+                       "Возр Рост ИСТ МАТ РУС  Ср.балл");
+        }
+
+        /// <summary>
+        /// Печать зап. книжки в центре консоли
+        /// Выравнивание столбцов по центру
+        /// </summary>
+        public void PrintCenter()
+        {
+            // Вычисляем ширину отступа, чтобы расположить таблицу по центру консоли
+            // Ширина таблицы 60 символов, если консоль уже, то отступ делаем равным 0 - пустая строка
+            // иначе, (ширина консоли - 60) / 2
+            // при нечётной ширине консоли, таблица будет на 1 символ левее,
+            // т.к. деление даст целое число
+            int consoleIndent = Console.WindowWidth > 60 ? 
+                                   (Console.WindowWidth - 60) / 2 :
+                                   0 ;
+            Console.WriteLine("\n Печать записной книжки по центру консоли");
+            Console.WriteLine("Ширина консоли: {0} символов", Console.WindowWidth);
+            Console.SetCursorPosition(consoleIndent, Console.CursorTop);
+            this.OutputHeader();
+
+            foreach (var student in this.Students)   // для каждого элемента записной книжки
+            {
+                Console.SetCursorPosition(consoleIndent, Console.CursorTop);        // отодвигаем курсор на отступ
+                student.OutputFormatCenter();        // печатаем форматированную по центру строку 
+                                                     // данных о студенте
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HomeWork_02
+{
+    /// <summary>
+    /// Класс, описывающий модель ученика
+    /// Поля данных и действия по выводу 
+    /// </summary>
+    /// <param name="Student"> Класс, описывающий модель ученика</param>
+
+    public class Student
+    {
+        /// <summary>
+        /// Объявляем переменные - поля значение соответствующим полям ученика
+        /// </summary>
+        string FirstName;   // имя
+        string LastName;    // фамилия
+        byte Age;         // возраст лет
+        byte Height;      // рост в см
+        byte History;     // оценка по истории 
+        byte Math;        // оценка по математике
+        byte RusLang;     // оценка по русскому языку
+        public float AvgMark;     // средняя оценка
+
+        /// <summary>
+        /// Конструктор, присваивающий значение полям ученика
+        /// </summary>
+        /// <param name="FirstName">Имя</param>
+        /// <param name="LastName">Фамилия</param>
+        /// <param name="Age">Возраст</param>
+        /// <param name="Height">Рост</param>
+        /// <param name="History">Оценка по истории</param>
+        /// <param name="Math">оценка по математике</param>
+        /// <param name="RusLang">оценка по русскому языку</param>
+        public Student(string FirstName, string LastName, byte Age, byte Height,
+                           byte History, byte Math, byte RusLang)
+        {
+            this.FirstName = FirstName; // Сохранить переданное значение имени
+            this.LastName = LastName;   // Сохранить переданное значение фамилии
+            this.Age = Age;             // Сохранить переданное значение возраста
+            this.Height = Height;       // Сохранить переданное значение роста
+            this.History = History;     // Сохранить переданную оценку по истории
+            this.Math = Math;           // Сохранить переданную оценку по математике
+            this.RusLang = RusLang;     // Сохранить переданную оценку по русскому языку
+            AvgMark = ((float)this.History + (float)this.Math + (float)this.RusLang) / (float)3.0; // вычисляем средний балл
+        }
+
+        /// <summary>
+        /// Метод неформатированый вывод данных о ученике в одну строку
+        /// </summary> 
+        public void OutputNoFormat()
+        {
+            Write("Ученик: " + FirstName + " " + LastName + " ");
+            Write(Age.ToString() + " лет, рост " + Height.ToString() + " см");
+            Write(". Баллы: история - " + History.ToString());
+            Write(", мат-ка - " + Math.ToString());
+            Write(", рус.язык - " + RusLang.ToString());
+            WriteLine(", средний: " + AvgMark.ToString());
+            return;
+        }
+
+        /// <summary>
+        /// Форматированный вывод данных в одну строку 
+        /// с выравниванием по правому краю каждой позиции
+        /// </summary>
+        public void OutputFormat()
+        {
+            string pattern = "Имя: {0,15} {1,15}, {2,5} лет, рост {3,3} см," +
+                             " история {4,2}, мат-ка {5,2}, рус.яз. {6,2}, ср. балл {7,4:#.##}";
+
+            WriteLine(pattern,
+                      FirstName, LastName, Age, Height,
+                      History, Math, RusLang, AvgMark);
+        }
+       
+        /// <summary>
+        /// Форматированный вывод данных
+        /// в одну строку для таблицы
+        /// с выравниванием по левому краю каждой позиции
+        /// </summary>
+        public string OutputFormatLeft()
+        {
+            return $"{FirstName,-15} {LastName,-15} {Age,-7} {Height,-7} " +
+                   $"{History,-7} {Math,-7} {RusLang,-7} {AvgMark.ToString("###.##"),-7}";
+        }
+
+
+        /// <summary>
+        /// Форматированный вывод данных c интерполяцией
+        /// в одну строку для таблицы
+        /// с выравниванием по центру каждой позиции
+        /// количество символов для каждой позиции.
+        /// </summary>
+        public void OutputFormatCenter()
+        {
+            // BFN - строка из необходимого количества пробелов ПЕРЕД именем в 15 символьном поле
+            String BFN = new String(' ', (15 - FirstName.Length) / 2);
+
+            // AFN - строка из необходимого количества пробелов ПОСЛЕ имени в 15 символьном поле
+            String AFN = ("").PadRight((15 - FirstName.Length - BFN.Length), ' ');
+
+            // BLN - строка из необходимого количества пробелов ПЕРЕД фамилией в 15 символьном поле
+            String BLN = new String(' ', (15 - LastName.Length) / 2);
+
+            // ALN - строка из необходимого количества пробелов ПОСЛЕ имени в 15 символьном поле
+            String ALN = ("").PadRight((15 - LastName.Length - BLN.Length), ' ');
+
+            // Формируем строку, где каждый элемент распологается по центру своего столбца
+            WriteLine($"{BFN}{FirstName}{AFN}" +       // ширина поля имени 15 символов 
+                      $"{BLN}{LastName}{ALN}" +        // ширина поля фамилии 15 символов
+                      $" {Age,2} " +            // обычно возраст 2 цифры. Расположим под "оз" из "Возр"
+                      $" {Height,4}" +          // обычно рост 3 цифры. Располжим под "ост" из "Рост"                   
+                      $" {History,2} " +        // оценка 1 цифра. Располжим под средней буквой С 
+                      $" {Math,2} " +           // оценка 1 цифра. Располжим под средней буквой А     
+                      $" {RusLang,2} " +        // оценка 1 цифра. Располжим под средней буквой У
+                      $"{AvgMark,7:#.##}");     // Ср.балл в формате #,## под р.ба
+        }
+    }
+}
